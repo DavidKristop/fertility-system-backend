ef40140b85539cc4bd5dcfd6674bca94414f018d
bb <99488111+ggbb0711@users.noreply.github.com>
2025-06-21 08:03:11 +0700

    Add global exception handling

--- src/main/java/com/group3/backend/controller/ScheduleController.java
+++ src/main/java/com/group3/backend/controller/ScheduleController.java
@@ -3,13 +3,15 @@ package com.group3.backend.controller;
 import com.group3.backend.dto.Response;
 import com.group3.backend.dto.request.Schedule.ScheduleResultRequest;
 import com.group3.backend.dto.response.Schedule.ScheduleResponse;
+import com.group3.backend.exception.ResourceNotFoundException;
 import com.group3.backend.mapper.ScheduleMapper;
 import com.group3.backend.model.Schedule;
 import com.group3.backend.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -30,23 +32,15 @@ public class ScheduleController {
             @PathVariable UUID doctorId,
             @RequestParam(required = false) Integer year,
             @RequestParam(required = false) Integer month) {
-        List<Schedule> schedules = scheduleService.getSchedulesByDoctorId(doctorId);
-        if(year == null) year = LocalDate.now().getYear();
-        if(month == null) month = LocalDate.now().getMonthValue();
-        LocalDate startOfMonth = LocalDate.of(year, month, 1);
-        LocalDate endOfMonth = startOfMonth.plusMonths(1).minusDays(1);
-        
-        schedules = schedules.stream()
-                .filter(schedule -> {
-                    LocalDate appointmentDate = schedule.getAppointmentDateTime().toLocalDateTime().toLocalDate();
-                    return !appointmentDate.isBefore(startOfMonth) && !appointmentDate.isAfter(endOfMonth);
-                })
-                .collect(Collectors.toList());
+        List<Schedule> schedules = scheduleService.getSchedulesByDoctorId(doctorId,year,month);
         
         List<ScheduleResponse> responses = schedules.stream()
                 .map(scheduleMapper::toResponse)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(new Response<>(responses,"Schedules retrieved successfully"));
+        return ResponseEntity.ok(new Response<>(
+            responses,
+            "Schedules retrieved successfully"
+        ));
     }
 
     @GetMapping("/patient/{patientId}")
@@ -54,34 +48,33 @@ public class ScheduleController {
             @PathVariable UUID patientId,
             @RequestParam(required = false) Integer year,
             @RequestParam(required = false) Integer month) {
-        List<Schedule> schedules = scheduleService.getSchedulesByPatientId(patientId);
-        if(year == null) year = LocalDate.now().getYear();
-        if(month == null) month = LocalDate.now().getMonthValue();
-        LocalDate startOfMonth = LocalDate.of(year, month, 1);
-        LocalDate endOfMonth = startOfMonth.plusMonths(1).minusDays(1);
-        
-        schedules = schedules.stream()
-                .filter(schedule -> {
-                    LocalDate appointmentDate = schedule.getAppointmentDateTime().toLocalDateTime().toLocalDate();
-                    return !appointmentDate.isBefore(startOfMonth) && !appointmentDate.isAfter(endOfMonth);
-                })
-                .collect(Collectors.toList());
+        List<Schedule> schedules = scheduleService.getSchedulesByPatientId(patientId,year,month);
         
         List<ScheduleResponse> responses = schedules.stream()
                 .map(scheduleMapper::toResponse)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(new Response<>(responses,"Schedules retrieved successfully"));
+        return ResponseEntity.ok(new Response<>(
+            responses,
+            "Schedules retrieved successfully"
+        ));
     }
 
     @GetMapping("/result/{scheduleId}")
     public ResponseEntity<Response<ScheduleResponse>> getScheduleResultById(@PathVariable UUID scheduleId) {
         Schedule schedule = scheduleService.getScheduleById(scheduleId);
-        return ResponseEntity.ok(new Response<>(scheduleMapper.toResponse(schedule),"Schedule result retrieved successfully"));
+        return ResponseEntity.ok(new Response<>(
+            scheduleMapper.toResponse(schedule),
+            "Schedule result retrieved successfully")
+        );
+            
     }
 
     @PostMapping("/result")
     public ResponseEntity<Response<ScheduleResponse>> addScheduleResult(@RequestBody ScheduleResultRequest request) {
         Schedule schedule = scheduleService.addScheduleResult(request);
-        return ResponseEntity.ok(new Response<>(scheduleMapper.toResponse(schedule),"Schedule result added successfully"));
+        return ResponseEntity.ok(new Response<>(
+            scheduleMapper.toResponse(schedule),
+            "Schedule result added successfully")
+        );
     }
 }
