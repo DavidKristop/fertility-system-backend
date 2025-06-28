da64fb1ab62787f22ff0cb64419326ac82ccd3e5
bb <99488111+ggbb0711@users.noreply.github.com>
2025-06-28 14:15:14 +0700

    Commit before merging to main

--- src/main/java/com/group3/backend/BackendApplication.java
+++ src/main/java/com/group3/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.group3.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Bangkok"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
