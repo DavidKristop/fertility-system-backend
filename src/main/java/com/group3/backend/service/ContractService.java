76cbfea27120dc51a45db5047249766c9cca8cb6
Le Bao Khang <khangle7405@gmail.com>
2025-06-28 19:41:12 +0700

    fix error

--- src/main/java/com/group3/backend/service/ContractService.java
+++ src/main/java/com/group3/backend/service/ContractService.java
@@ -1,49 +1,5 @@
 package com.group3.backend.service;
 
-<<<<<<< HEAD
-import com.group3.backend.model.*;
-import com.group3.backend.repository.ContractRepository;
-import com.group3.backend.repository.TreatmentRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.UUID;
-
-@Service
-@RequiredArgsConstructor
-public class ContractService {
-
-    private final ContractRepository contractRepository;
-    private final TreatmentRepository treatmentRepository;
-
-    public List<Contract> getContractsByPatientId(UUID patientId) {
-        return contractRepository.findByTreatment_Patient_Id(patientId);
-    }
-
-    public Contract createContract(UUID treatmentId, Timestamp deadline, String pdfUrl) {
-        Treatment treatment = treatmentRepository.findById(treatmentId)
-                .orElseThrow(() -> new IllegalArgumentException("Treatment not found"));
-
-        Contract contract = Contract.builder()
-                .treatment(treatment)
-                .contractUrl(pdfUrl)
-                .signDeadline(deadline)
-                .isSigned(false)
-                .build();
-
-        return contractRepository.save(contract);
-    }
-
-    public Contract signContract(UUID contractId) {
-        Contract contract = contractRepository.findById(contractId)
-                .orElseThrow(() -> new IllegalArgumentException("Contract not found"));
-
-        contract.setIsSigned(true);
-        return contractRepository.save(contract);
-    }
-=======
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -91,7 +47,7 @@ public class ContractService {
         if(contract.getTreatment().getPatient().getId() != patientId){
             throw new UnauthorizedAccessException("You are not authorized to sign this contract");
         }
-        contract.setSigned(true);
+        contract.setIsSigned(true);
         createPaymentBasedOnPaymentMode(contract.getTreatment());
         treatment.setStatus(Treatment.Status.IN_PROGRESS);
         contract.setTreatment(treatment);
@@ -137,5 +93,4 @@ public class ContractService {
                 .map(TreatmentPhase::getTotalAmount)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
->>>>>>> master
 }
