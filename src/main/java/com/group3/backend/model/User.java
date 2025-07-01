ab6b71676cb1faf0a5df797b4e2956788a36d68a
Le Bao Khang <khangle7405@gmail.com>
2025-07-01 09:03:36 +0700

    Merge with master

--- src/main/java/com/group3/backend/model/User.java
+++ src/main/java/com/group3/backend/model/User.java
@@ -20,7 +20,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
