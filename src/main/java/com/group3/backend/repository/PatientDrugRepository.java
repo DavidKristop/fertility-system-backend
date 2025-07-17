9d43b33eff11de98b04528580df5e829e8442519
bb <99488111+ggbb0711@users.noreply.github.com>
2025-07-17 07:49:46 +0700

    Add in a way to fetch drug and schedule for patient

--- src/main/java/com/group3/backend/repository/PatientDrugRepository.java
+++ src/main/java/com/group3/backend/repository/PatientDrugRepository.java
@@ -1,14 +1,15 @@
 package com.group3.backend.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.group3.backend.model.Drug;
 import com.group3.backend.model.PatientDrug;
 
 public interface PatientDrugRepository extends JpaRepository<PatientDrug, UUID> {
     List<PatientDrug> findByIdIn(List<UUID> ids);
+
+    List<PatientDrug> findByAssignDrugTreatmentPhaseTreatmentPatientIdAndStartDateBetween(UUID patientId,LocalDate start,LocalDate end);
 }
